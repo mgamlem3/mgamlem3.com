1cf56ec7d03e167dbbca5daa61fc29d558179e03
Michael Gamlem III <mgamlem3@gmail.com>
2021-01-10 13:48:03 -0800

    Send compressed file if available

--- server/server.ts
+++ server/server.ts
@@ -7,21 +7,23 @@
 import path from "path";
 import express, { Request, Response } from "express";
 import fs from "fs";
-import { check } from "express-validator";
 
 const PORT = 3000;
 const DIST_DIR = path.join(__dirname);
 
 const app = express();
 
-app.use(express.static(DIST_DIR));
-
 app.get(/.(jpg|png|js|css)$/, (req: Request, res: Response) => {
-	const cleanUrl: string = sanitizeUrl(req.url);
+	sanitizeUrl(req);
 
 	checkContentEncoding(req, res);
-	if (checkIfFileAllowed(cleanUrl)) sendFile(cleanUrl, res);
+	if (checkIfFileAllowed(req)) sendFile(req, res);
 	else res.status(404).end();
+});
+
+app.get(/.ico$/, (req: Request, res: Response) => {
+	req.url = "/favicon.ico";
+	sendFile(req, res);
 });
 
 app.get("/", (req: Request, res: Response) => {
@@ -29,7 +31,7 @@ app.get("/", (req: Request, res: Response) => {
 
 	req.url = INDEX_URL;
 	checkContentEncoding(req, res);
-	sendFile(INDEX_URL, res);
+	sendFile(req, res);
 });
 
 app.listen(PORT, () => {
@@ -37,7 +39,8 @@ app.listen(PORT, () => {
 });
 
 const checkContentEncoding = (req: Request, res: Response) => {
-	if (req.acceptsEncodings("br")) {
+	if (req.url.match(/.jpg|.png$/)) return;
+	else if (req.acceptsEncodings("br")) {
 		req.url += ".br";
 		res.set("Content-Encoding", "br");
 	} else if (req.acceptsEncodings("gzip")) {
@@ -46,17 +49,31 @@ const checkContentEncoding = (req: Request, res: Response) => {
 	}
 };
 
-const checkIfFileAllowed = (fileName: string): boolean => {
-	if (fs.existsSync(DIST_DIR + fileName)) {
-		return true;
+const getContentType = (fileName: string) => {
+	if (fileName.match(/.js(.br|.gz)?$/)) return "text/javascript";
+	else if (fileName.match(/.css(.br|.gz)?$/)) return "text/css";
+	else if (fileName.match(/.html(.br|.gz)?$/)) return "text/html";
+	else if (fileName.match(/.jpg(.br|.gz)?|.jpeg(.br|.gz)?$/))
+		return "image/jpeg";
+	else if (fileName.match(/.png(.br|.gz)?$/)) return "image/png";
+	else if (fileName.match(/.ico$/)) return "image/vnd.microsoft.icon";
+	else {
+		console.error("Broken: ", fileName);
+		return "";
 	}
-	return false;
 };
 
-const sendFile = (path: string, res: Response) => {
-	res.sendFile(path);
+const checkIfFileAllowed = (req: Request): boolean => {
+	if (fs.existsSync(DIST_DIR + req.url)) {
+		return true;
+	} else return false;
 };
 
-const sanitizeUrl = (url: string): string => {
-	return check(url).trim().escape().toString();
+const sendFile = (req: Request, res: Response) => {
+	res.set("Content-Type", getContentType(req.url));
+	res.sendFile(DIST_DIR + req.url);
+};
+
+const sanitizeUrl = (req: Request): void => {
+	req.url = req.url.trim().toString();
 };
